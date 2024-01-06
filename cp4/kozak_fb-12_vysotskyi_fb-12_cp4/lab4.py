d0daf4199e75f793f3fb915794167af78cebfe1a
chitahero <vladkozak2306@gmail.com>
2024-01-06 17:06:53 +0200

    схема горнера

--- cp4/kozak_fb-12_vysotskyi_fb-12_cp4/lab4.py
+++ cp4/kozak_fb-12_vysotskyi_fb-12_cp4/lab4.py
@@ -1,2 +1,23 @@
-def generate_prime_number(lowest: int, highest: int) -> int:
+def generate_prime_number(lowest: int, highest: int) -> str:
     pass
+
+def decimal_to_binary(a: int) -> str:
+    binary_a = ""
+    while a > 0:
+        remainder = a % 2
+        binary_a = str(remainder) + binary_a
+        a //= 2
+    return binary_a if binary_a else "0"
+
+
+def horner_power(x: int, a: int, m: int) -> int:
+    #  x^a (mod m)
+    result = 1
+    binary_a = decimal_to_binary(a)
+
+    for bit in binary_a:
+        result = (result * result) % m
+        if bit == '1':
+            result = (result * x) % m
+
+    return result
